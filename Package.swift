d840712e5fdd17383326e31941a61c76773231f3
wangyiwei <wangyw.dev@outlook.com>
2022-07-21 16:24:29 +0800

    fixed?

--- Package.swift
+++ Package.swift
@@ -9,17 +9,17 @@ let package = Package(
         // Products define the executables and libraries a package produces, and make them visible to other packages.
         .library(
             name: "Invisibility",
-            targets: ["Invisibility"]),
+            targets: ["Wallpaper"]),
     ],
     dependencies: [],
     targets: [
         // Targets are the basic building blocks of a package. A target can define a module or a test suite.
         // Targets can depend on other targets in this package, and on products in packages this package depends on.
         .target(
-            name: "Invisibility",
+            name: "Wallpaper",//"Invisibility",
             dependencies: []),
-        .testTarget(
-            name: "InvisibilityTests",
-            dependencies: ["Invisibility"]),
+        //.testTarget(
+            //name: "InvisibilityTests",
+            //dependencies: ["Invisibility"]),
     ]
 )
--- Sources/Invisibility/Invisibility.swift
+++
@@ -1,6 +0,0 @@
-public struct Invisibility {
-    public private(set) var text = "Hello, World!"
-
-    public init() {
-    }
-}
